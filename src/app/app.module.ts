606f1cc8c0423a6ff5b7787caf94352060ee298c
Nejciny <116084048+Nejciny@users.noreply.github.com>
2023-06-18 23:19:30 +0200

    create app
    app creation,
    weather search functionality ( daily weather --> 5day report not working ),
    basic styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { WeatherComponent } from './weather/weather.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    WeatherComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/weather/weather.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import * as moment from 'moment';
+
+@Component({
+  selector: 'app-weather',
+  templateUrl: './weather.component.html',
+  styleUrls: ['./weather.component.scss']
+})
+
+
+export class WeatherComponent implements OnInit {
+
+  isLoading: boolean = false;
+  weatherData: any;
+  city: string = 'maribor';
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+  }
+
+  async searchWeather(): Promise<void> {
+    if (this.city.trim() === '') {
+      return;
+    }
+
+    this.isLoading = true;
+    this.weatherData = null;
+
+    // Replace 'YOUR_API_KEY' and 'YOUR_API_URL' with the appropriate values
+    const apiKey = '1b94df0ce07291e14d52f44a6761b5de';
+    // const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${this.city}`;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${apiKey}&units=metric`
+
+    try {
+      const data: any = await this.http.get(apiUrl).toPromise();
+      
+      
+      this.weatherData = {
+        ...data,
+        retrievalTime: moment().format('DD.MM.YYYY ')
+      };
+
+      setTimeout(()=>{
+        this.isLoading = false;
+      }, 1000); // delay of 1seconds so we can see the loading text
+
+      console.log(data);
+
+      console.log(this.weatherData);
+
+
+    } catch (error) {
+      console.log('An error occurred while fetching weather data:', error);
+      this.isLoading = false;
+    }
+  }
+
+}
